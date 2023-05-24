2c9637d4ff39be5514cecac162600cf86b1a1981
Leonardo Marziali <marzialileonardo862@gmail.com>
2023-05-24 10:55:31 +0200

    Remove .editorconfig

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,14 +1,10 @@
 module.exports = {
-  semi: true,
-  tabWidth: 2,
-  printWidth: 100,
   singleQuote: true,
   trailingComma: 'none',
   bracketSameLine: true,
   importOrder: ['^components/(.*)$', '^[./]'],
   importOrderSeparation: true,
   importOrderSortSpecifiers: true,
-  endOfLine: 'lf',
   plugins: [
     require.resolve('@trivago/prettier-plugin-sort-imports'),
     require.resolve('prettier-plugin-organize-imports'),
